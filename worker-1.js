e6a4873db6e8dc149b9b8ceaa5100476d972340d
gonzalojavierlopez95-rgb <gonzalojavierlopez95@gmail.com>
2026-09-13 14:36:38 -0300

    Delete worker-1.js

--- worker-1.js
+++
@@ -1,8 +0,0 @@
-export default {
-  async fetch(request) {
-    return new Response(
-      JSON.stringify({ ok: true, mensaje: "Nova API funcionando" }),
-      { headers: { "Content-Type": "application/json" } }
-    );
-  },
-};
